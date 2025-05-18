c83366e6a76fcea026437988a3cabf7895357d52
BibasYonghang <bibasyonghang100@gmail.com>
2025-05-18 13:53:54 +0545

    changes in .gitignore

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,9 +1,9 @@
-import Alert from './components/Alert';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import About from './components/About';
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Alert from './components/Alert';
+import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -18,31 +18,31 @@ function App() {
     if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#031535';
-      showAlert("Dark Mode has been Enabled", "success");
+      showAlert('Dark Mode has been Enabled', 'success');
       document.title = 'TextUtils - Dark Mode';
     } else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
-      showAlert("Light Mode has been Enabled", "success");
+      showAlert('Light Mode has been Enabled', 'success');
       document.title = 'TextUtils - Light Mode';
     }
   };
 
   return (
-    <>
-      <Router>
-        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-        <Alert alert={alert} />
-        <div className="container my-3">
-          <Routes>
-            <Route path="/about" element={<About />} />
-            <Route path="/" element={<TextForm showAlert={showAlert} heading='Enter the text to Analyze Below:' mode={mode} />} />
-          </Routes>
-        </div>
-      </Router>
-    </>
+    <Router>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        <Routes>
+          <Route
+            path="/"
+            element={<TextForm showAlert={showAlert} heading="Enter the text to Analyze Below:" mode={mode} />}
+          />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
-
